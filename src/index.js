ba39bfbf535b597560fc40269a6fea31ce7ca88d
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2019-04-15 15:42:02 +0200

    feat: update to new transformation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const debug = require('debug')('cloudinary:log');
 
 const { Promise } = bluebird;
 bluebird.promisifyAll(cloudinary);
-const MAX_SIZE = 960;
+const MAX_SIZE = 400;
 
 const sleep = time => new Promise(resolve => setTimeout(() => resolve(), time));
 
@@ -12,11 +12,11 @@ let total = 0;
 let counter = 0;
 
 async function reuploadImage(resource) {
-  if (resource.width > MAX_SIZE || resource.height > MAX_SIZE) {
+  if ((resource.width > MAX_SIZE || resource.height > MAX_SIZE) && resource.type === 'upload') {
     counter += 1;
     await cloudinary.v2.uploader.upload(resource.url, {
       public_id: resource.public_id,
-      transformation: 'downsizing',
+      transformation: 'downsizing2',
       invalidate: true,
       overwrite: true,
     });
@@ -26,19 +26,24 @@ async function reuploadImage(resource) {
 async function runner() {
   let nextCursor = null;
   do {
-    const result = await cloudinary.v2.api.resources({
-      max_results: 500,
-      next_cursor: nextCursor,
-    });
-    nextCursor = result.next_cursor;
+    try {
+      const result = await cloudinary.v2.api.resources({
+        max_results: 500,
+        next_cursor: nextCursor,
+      });
+      nextCursor = result.next_cursor;
 
-    const transformBatch = result.resources.map(resource => reuploadImage(resource));
+      const transformBatch = result.resources.map(resource => reuploadImage(resource));
 
-    await Promise.all(transformBatch);
+      await Promise.all(transformBatch);
 
-    total += result.resources.length;
-    if (total % 1000 === 0) {
-      debug('transformed', counter, 'of', total, 'at', nextCursor);
+      total += result.resources.length;
+      if (total % 1000 === 0) {
+        debug('transformed', counter, 'of', total, 'at', nextCursor);
+      }
+    } catch (err) {
+      console.error('Error occurred');
+      console.error(err);
     }
   } while (nextCursor);
 
